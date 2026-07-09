434f4c4db71a55e4fab05e87c8d3869632532b1a
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-07-09 19:58:23 +0300

    fix: solved some problems

--- blog/forms.py
+++ blog/forms.py
@@ -6,12 +6,21 @@ from .models import Commentary
 class CommentaryForm(forms.ModelForm):
     class Meta:
         model = Commentary
-        fields = ("content",)
+        fields = ["content"]
         widgets = {
             "content": forms.Textarea(
-                attrs={
-                    "rows": 3,
-                    "placeholder": "Leave a comment..."
-                }
+                attrs={"rows": 3, "placeholder": "Write a comment..."}
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not self.user or not self.user.is_authenticated:
+            raise forms.ValidationError(
+                "Anonymous users are prohibited from submitting comments."
+            )
+        return cleaned_data
